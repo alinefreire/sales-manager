eb5fa2bd7ae72ec7716f669ccd5b33f2d501485a
Line Freire <alinefreire2312@gmail.com>
2020-07-14 03:30:53 -0300

    Incluso arquivo de config do database mongoDB

--- src/config/database.php
+++ src/config/database.php
@@ -1,1 +1,25 @@
 <?php
+
+return [
+    'default' => 'mongodb',
+    'connections' => [
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'dsn' => env('DB_DSN'),
+            'database' => env('DB_DATABASE'),
+            'options' => [
+                'database' => 'admin'
+            ]
+        ],
+        'test' => [
+            'driver' => 'mongodb',
+            'dsn' => env("DB_TEST_DSN"),
+            'database' => env('DB_TEST_DATABASE'),
+            'options' => [
+                'database' => 'admin'
+            ]
+        ],
+
+    ],
+    'migrations' => 'migrations',
+];
